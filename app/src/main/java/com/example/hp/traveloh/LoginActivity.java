a8b9f7e221337a49aa3c1688232b8e9466ebb9e9
Rahul <rahulsakaria97@gmail.com>
2017-01-26 21:06:38 +0530

    Intent for UserInput Added

--- app/src/main/java/com/example/hp/traveloh/LoginActivity.java
+++ app/src/main/java/com/example/hp/traveloh/LoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.hp.traveloh;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+        public void SignUp(View view){
+            Intent intent = new Intent(this,UserInputActivity.class);
+            startActivity(intent);
     }
 }
+
