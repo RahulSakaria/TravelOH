32aae058e4df78555340fdeaaa22a2e1ed2fbbf1
Rahul <rahulsakaria97@gmail.com>
2017-01-30 18:16:05 +0530

    SplashScreen Added

--- app/src/main/java/com/example/hp/traveloh/LoginActivity.java
+++ app/src/main/java/com/example/hp/traveloh/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.hp.traveloh;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,13 +10,21 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-
+    private static int SPLASH_TIME_OUT = 4000;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(LoginActivity.this, SplashActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
 
     }
     public void LogIn(View view){
