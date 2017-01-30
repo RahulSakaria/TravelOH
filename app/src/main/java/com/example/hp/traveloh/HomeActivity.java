148abdef01516d647b6b67cc0a756737a648478d
Rahul <rahulsakaria97@gmail.com>
2017-01-30 16:14:38 +0530

    HomeActivity Added 1.0

---
+++ app/src/main/java/com/example/hp/traveloh/HomeActivity.java
@@ -0,0 +1,8 @@
+package com.example.hp.traveloh;
+
+/**
+ * Created by HP on 30-01-2017.
+ */
+
+public class HomeActivity {
+}
--- app/src/main/java/com/example/hp/traveloh/LoginActivity.java
+++ app/src/main/java/com/example/hp/traveloh/LoginActivity.java
@@ -4,14 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    EditText emailid = (EditText) findViewById(R.id.enter_password);
+    EditText password = (EditText) findViewById(R.id.enter_password);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+    }
+    public void LogIn(View view){
+        if(emailid.getText().equals("rahulsakaria97@gmail.com") && password.getText().equals("password"))
+        {
+            Toast.makeText(this,"Login Successful",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this,HomeActivity.class);
+            startActivity(intent);
+        }
+        else
+        {
+            Toast.makeText(this,"Enter valid email/password",Toast.LENGTH_SHORT).show();
+        }
     }
         public void SignUp(View view){
             Intent intent = new Intent(this,UserInputActivity.class);
