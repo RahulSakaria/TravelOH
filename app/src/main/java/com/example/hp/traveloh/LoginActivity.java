b84d94698a41a10226c9c329529756a6e8e5e097
Rahul <rahulsakaria97@gmail.com>
2017-01-30 21:08:39 +0530

    SplashScreen Added 1.1

--- app/src/main/java/com/example/hp/traveloh/LoginActivity.java
+++ app/src/main/java/com/example/hp/traveloh/LoginActivity.java
@@ -1,13 +1,15 @@
 package com.example.hp.traveloh;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
 
 
@@ -15,6 +17,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
 
 
--- app/src/main/java/com/example/hp/traveloh/MainActivity.java
+++ app/src/main/java/com/example/hp/traveloh/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.hp.traveloh;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     private static int SPLASH_TIME_OUT = 4000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
         new Handler().postDelayed(new Runnable() {
             @Override
